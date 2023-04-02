58dfa82768aade13fc3065f2f9969882e1451c5d
André Liberato <andre.liberato@outlook.com.br>
2023-04-02 13:45:29 -0300

    First commit

---
+++ src/main/java/com/pds/postocerto/PostocertoApplication.java
@@ -0,0 +1,13 @@
+package com.pds.postocerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostocertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostocertoApplication.class, args);
+	}
+
+}
