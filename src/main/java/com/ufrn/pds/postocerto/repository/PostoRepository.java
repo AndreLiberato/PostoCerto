aac4d30e769d5d3ca6f268fa75ad574b804419af
André Liberato <andre.liberato@outlook.com.br>
2023-04-11 22:02:13 -0300

    PostoRepository

---
+++ src/main/java/com/ufrn/pds/postocerto/repository/PostoRepository.java
@@ -0,0 +1,8 @@
+package com.ufrn.pds.postocerto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ufrn.pds.postocerto.model.Posto;
+
+public interface PostoRepository extends JpaRepository<Posto, Long>{
+  
+}
