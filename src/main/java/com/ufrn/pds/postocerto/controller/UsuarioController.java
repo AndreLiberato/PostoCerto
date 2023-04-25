3f98452ac0e06457f5ae9550dde15cfd9df36253
André Liberato <andre.liberato@outlook.com.br>
2023-04-25 13:09:10 -0300

    Alterações e ajustes

--- src/main/java/com/ufrn/pds/postocerto/controller/UsuarioController.java
+++ src/main/java/com/ufrn/pds/postocerto/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import com.ufrn.pds.postocerto.model.Usuario;
 import com.ufrn.pds.postocerto.service.IUsuarioService;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
